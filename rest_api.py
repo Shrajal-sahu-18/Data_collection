9f6bd5e07758cc27846c0a6423f35fac402bf075
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-12 23:21:54 +0530

    Convert data into DataFrame

--- rest_api.py
+++ rest_api.py
@@ -1,6 +1,9 @@
 import requests
+import pandas as pd
 url = "https://stephen-king-api.onrender.com/api/books"
 res = requests.get(url)
 json_data = res.json()
-print(json_data)
 
+
+df  = pd.json_normalize(json_data["data"])
+print(df)
