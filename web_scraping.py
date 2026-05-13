8a81351b3c2171b67a9adb0b841b9bd9823eb95e
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-13 23:37:45 +0530

    import beautifulsoup

--- web_scraping.py
+++ web_scraping.py
@@ -3,3 +3,5 @@ url = "https://www.scrapethissite.com/pages/simple/"
 res = requests.get(url)
 with open("scraped_data/data1.html","w",encoding = "utf-8") as f:
     f.write(res.text)
+
+from bs4 import BeautifulSoup
