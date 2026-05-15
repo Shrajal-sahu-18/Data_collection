48e6a4dfd7ca317638eb27dbc437e4256d8d3d0a
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-15 22:24:01 +0530

    load data with requests

--- scraped_data.py
+++ scraped_data.py
@@ -2,3 +2,9 @@
 import requests
 from bs4 import BeautifulSoup 
 import pandas as pd
+
+page_count = 1
+while True:
+    url = f"https://quotes.toscrape.com/page/{page_count}/"
+    res = requests.get(url)
+    
