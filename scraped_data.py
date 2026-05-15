315cb38a301c8316af9e6bea5e483ee3ab088479
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-15 22:31:55 +0530

    using BeautifulSoup

--- scraped_data.py
+++ scraped_data.py
@@ -7,4 +7,8 @@ page_count = 1
 while True:
     url = f"https://quotes.toscrape.com/page/{page_count}/"
     res = requests.get(url)
+    soup = BeautifulSoup(res.text,"lxml")
+    quotes = soup.select("div.quote")
+    print(quotes)
+    break
     
